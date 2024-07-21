579da364db05f1f45ce23e379ccc09f5ef3dfaa5
Aaron <aaronbaron@hotmail.co.uk>
2024-07-21 16:19:15 +0100

    utilise refactored object class, dynamic details box to limit the amount of wasted ndoes used

---
+++ core/factory/object_factory.gd
@@ -0,0 +1,12 @@
+extends Node
+func create_placeable_object(object_res: PackedScene, parameters: Dictionary):
+	var new_object: PlaceableObject = object_res.instantiate()
+	new_object.details.internal_name = parameters["name_id"]
+	new_object.position =  parameters["tilemap_coord"]
+	new_object.visual.atlas_index = parameters["palette_index"]
+	new_object.set_texture(parameters["atlas"])
+	new_object.set_region(parameters["region"])
+	new_object.set_parent(parameters["parent"])
+	new_object.mode = parameters["mode"]
+	#	new_object.set_real_name()
+	return new_object
--- core/object/interaction.gd
+++ core/object/interaction.gd
@@ -3,13 +3,14 @@ class_name Interaction
 signal mouse_entered_rect()
 signal mouse_left_rect()
 signal selected_toggled(is_selected)
+signal is_selected()
 signal log_event(msg)
 
 
 var rect: Rect2
 var mouse_hover: bool = false
 var selected: bool = false
-var is_dragged: bool = false
+var is_dragging: bool = false
 
 func _init(rect: Rect2):
 	self.rect = rect
@@ -20,15 +21,19 @@ func update(mousePos: Vector2, position: Vector2):
 func check_input(event: InputEvent):
 	var select_clicked = event.is_action_released("select") 
 	if select_clicked && mouse_hover:
-		toggle_selected()
+		select()
 
 func toggle_mouse_hover():
 	self.mouse_hover = !mouse_hover
 
 func toggle_selected():
-	is_dragged = false
 	self.selected = !selected
 	selected_toggled.emit(selected)
+	log_event.emit(" selected", selected)
+
+func select():
+	self.selected = true
+	is_selected.emit()
 	log_event.emit(" selected", selected)
 
 func set_mouse_hover(value: bool):
