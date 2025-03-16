9d9245953526dd3e6148706dcc48958977e4a2df
Aaron <aaronbaron@hotmail.co.uk>
2025-03-16 18:02:03 +0000

    move console folder -> core/ -  merge extension folder

--- core/console/server_commands.gd
+++ core/console/server_commands.gd
@@ -4,6 +4,8 @@ var status_format = "\nPeer ID: %s\nPeer Address: %s\nPeer Port: %s"
 
 func status(server: Server):
 	var target = server.target
-	return status_format % [server.peer.get_peer(target), 
-							server.peer.get_peer_address(target), 
-							server.peer.get_peer_port(target)]
+	return status_format % [
+		server.peer.get_peer(target), 
+		server.peer.get_peer_address(target), 
+		server.peer.get_peer_port(target)
+	]
